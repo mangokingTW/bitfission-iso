1ae7aaada172c082889f1652b95039103470c3ff
mangokingtw <yench@cs.nctu.edu.tw>
2017-11-09 02:45:57 +0800

    add default user [admin]

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -11,6 +11,11 @@ usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
+! id admin && useradd -m -p "" -g users -G "adm,audio,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh admin
+cp -aT /etc/skel/ /home/admin
+chown -R admin:users /home/admin
+chmod 700 /home/admin
+
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
@@ -18,6 +23,7 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
